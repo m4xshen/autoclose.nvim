8611fd5e652015affea774c7b7c50d1ef2630696
max <max22336733@gmail.com>
2022-02-07 17:26:00 +0800

    basic autoclose function

---
+++ lua/autoclose/init.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_keymap("i", "{", "{}<ESC>i", {noremap = true})
+vim.api.nvim_set_keymap("i", "[", "[]<ESC>i", {noremap = true})
+vim.api.nvim_set_keymap("i", "(", "()<ESC>i", {noremap = true})
+vim.api.nvim_set_keymap("i", "'", "''<ESC>i", {noremap = true})
+vim.api.nvim_set_keymap("i", "\"", "\"\"<ESC>i", {noremap = true})
